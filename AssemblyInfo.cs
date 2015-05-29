f6d8b69c0ef8b9c1d3b550400a1cae2be51764aa
Oleg Sych <oleg_sych@hotmail.com>
2015-05-28 21:16:50 -0700

    - Initial solution skeleton.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Oleg Sych">
+//  Copyright © 2015 Oleg Sych
+// </copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif 
+
+[assembly: AssemblyProduct("Visual Studio host adapter for MSTest")]
+[assembly: AssemblyCompany("Oleg Sych")]
+[assembly: AssemblyCopyright("Copyright @ 2015 Oleg Sych")]
+[assembly: ComVisible(false)]
+
+// Version attributes are patched by build
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Runner/Package.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="Package.cs" company="Oleg Sych">
+//  Copyright © 2015 Oleg Sych
+// </copyright>
+
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.TestTools.TestAdapter;
+
+namespace TestAdapterHost
+{
+    /// <summary>
+    /// Provides a service that allows obtaining an <see cref="ITestAdapter"/> and 
+    /// running tests in the Visual Studio process.
+    /// </summary>
+    [Guid("cabe79df-1d59-43c7-9354-f17310f19c8a")]
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    public sealed class Package : Microsoft.VisualStudio.Shell.Package
+    {
+    }
+}
