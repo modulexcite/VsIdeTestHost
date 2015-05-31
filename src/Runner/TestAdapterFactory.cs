05374b6da5fe7ea4e27bde718dcefb84a4a84f87
Oleg Sych <oleg_sych@hotmail.com>
2015-05-31 12:58:08 -0700

    - Implement TestAdapterFactory.

--- src/Runner/TestAdapterFactory.cs
+++ src/Runner/TestAdapterFactory.cs
@@ -2,6 +2,7 @@
 //  Copyright © 2015 Oleg Sych
 // </copyright>
 
+using System;
 using Microsoft.VisualStudio.TestTools.TestAdapter;
 
 namespace VsIdeTestHost.Runner
@@ -9,11 +10,15 @@ namespace VsIdeTestHost.Runner
     /// <summary>
     /// Represents an object responsible for creating <see cref="ITestAdapter"/> instances.
     /// </summary>
-    public abstract class TestAdapterFactory
+    public class TestAdapterFactory
     {
         /// <summary>
         /// Creates an <see cref="ITestAdapter"/> instance with the given type name.
         /// </summary>
-        public abstract ITestAdapter CreateTestAdapter(string typeName);
+        public virtual ITestAdapter CreateTestAdapter(string typeName)
+        {
+            Type adapterType = Type.GetType(typeName);
+            return (ITestAdapter)Activator.CreateInstance(adapterType);
+        }
     }
 }
---
+++ test/Runner.Tests/TestAdapterFactoryTest.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="TestAdapterFactoryTest.cs" company="Oleg Sych">
+//  Copyright © 2015 Oleg Sych
+// </copyright>
+
+using Microsoft.VisualStudio.TestTools.Common;
+using Microsoft.VisualStudio.TestTools.Execution;
+using Microsoft.VisualStudio.TestTools.TestAdapter;
+using Xunit;
+
+namespace VsIdeTestHost.Runner.Tests
+{
+    public class TestAdapterFactoryTest
+    {
+        [Fact]
+        public void CreateTestAdapterReturnsInstanceOfTypeWithGivenName()
+        {
+            var factory = new TestAdapterFactory();
+            string stubAdapterTypeName = typeof(StubTestAdapter).AssemblyQualifiedName;
+            ITestAdapter adapter = factory.CreateTestAdapter(stubAdapterTypeName);
+            Assert.IsType<StubTestAdapter>(adapter);
+        }
+
+        public class StubTestAdapter : ITestAdapter
+        {
+            void IBaseAdapter.AbortTestRun()
+            {
+            }
+
+            void IBaseAdapter.Cleanup()
+            {
+            }
+
+            void ITestAdapter.Initialize(IRunContext runContext)
+            {
+            }
+
+            void IBaseAdapter.PauseTestRun()
+            {
+            }
+
+            void ITestAdapter.PreTestRunFinished(IRunContext runContext)
+            {
+            }
+
+            void ITestAdapter.ReceiveMessage(object message)
+            {
+            }
+
+            void IBaseAdapter.ResumeTestRun()
+            {
+            }
+
+            void IBaseAdapter.Run(ITestElement testElement, ITestContext testContext)
+            {
+            }
+
+            void IBaseAdapter.StopTestRun()
+            {
+            }
+        }
+    }
+}
